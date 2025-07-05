cfca0120183f9419583dc24944555bbfa7332a57
Anwana Ntofon <anwanantofon@gmail.com>
2025-07-05 15:20:09 -0700

    updated build to include concept2 html file

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        concept2: resolve(__dirname, 'concept2.html')
+      }
+    }
+  }
+})
